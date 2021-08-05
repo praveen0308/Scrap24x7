6197f22ed43e3d0629d086770961848655fe2e83
ADMIN <praveen0308>
2021-08-05 11:40:28 +0530

    5 august 2021

--- app/src/main/java/com/jmm/brsap/scrap24x7/di/NetworkModule.kt
+++ app/src/main/java/com/jmm/brsap/scrap24x7/di/NetworkModule.kt
@@ -1,12 +1,10 @@
 package com.jmm.brsap.scrap24x7.di
 
-import android.content.Context
 import com.jmm.brsap.scrap24x7.network.ApiService
 import com.jmm.brsap.scrap24x7.util.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
